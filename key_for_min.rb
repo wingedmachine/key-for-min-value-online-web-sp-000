80831e2489b562759d7d136bdaf5f74c6fff58c2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 00:36:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.size == 0
 
-  min, min_key
+  min, min_key = nil
   name_hash.each do |key, value|
     if min == nil || value < min
       min = value
